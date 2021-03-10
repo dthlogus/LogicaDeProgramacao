cc4344b124675f1e10407a21d23d8c2d81ae3535
Matheus Ribeiro <matheus.junio159@gmail.com>
2021-03-10 18:35:13 -0300

    A classe principal do java

---
+++ algorithmWithJava/src/algorithmWithJava/Master.java
@@ -0,0 +1,18 @@
+package algorithmWithJava;
+
+import java.io.IOException;
+
+public class Master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		In test = new In();
+		try {
+			test.InWithConsole();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
