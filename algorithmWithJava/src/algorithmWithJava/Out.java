0a77cb67b880f3496a486a02842c375f0a4fcfad
Matheus Ribeiro <matheus.junio159@gmail.com>
2021-03-11 08:18:44 -0300

    Adicionando novas informações é uma saída por println e interface gráfica

--- algorithmWithJava/src/algorithmWithJava/Out.java
+++ algorithmWithJava/src/algorithmWithJava/Out.java
@@ -3,6 +3,8 @@ package algorithmWithJava;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 /*
  * Aqui mostra um sistema de formação que não precisa ser concatenado com '+'
  * no lugar que você deseja mostrar as variáveis na saída você adiciona %<o formato>
@@ -13,6 +15,7 @@ import java.util.Scanner;
  * %t = date
  * %c = char
  * %b = boolean
+ * Também usando o JOptionPane.showMessageDialog para retorna uma saída por interface gráfica.
  */
 
 public class Out {
@@ -29,6 +32,10 @@ public class Out {
 		System.out.println("What is your age? ");
 		age = scan.nextInt();
 		
+		System.out.println("Your name is: "+name+"\nYour  age is: "+age+"\nYour wage is: "+wage+"\n\n");
+		
 		System.out.printf("Your name is: %s \nYour age is: %d \nYour wage is: %f", name, age, wage);
+		
+		JOptionPane.showMessageDialog(null, "Your name is: "+name+"\nYour  age is: "+age+"\nYour wage is: "+wage);
 	}
 }
